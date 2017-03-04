b19776da46be440328aa70de57382d3351d2d419
pufko <pufan4eg@gmail.com>
2017-03-04 03:16:19 +0200

    gearbest grabber

---
+++ src/Test.java
@@ -0,0 +1,57 @@
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        List<Article> articleList = new ArrayList<>();
+        Document doc = Jsoup.connect("http://www.gearbest.com/temperature-control-mods/pp_423259.html").get();
+        Elements orgp = doc.getElementsByAttribute("orgp");
+        String price = orgp.get(0).attr("orgp");
+        System.out.println(price);
+       // System.out.println(orgp);
+//
+        articleList.forEach(System.out::println);
+
+    }
+}
+
+class Article {
+    private String url;
+    private String name;
+
+    public Article(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "url='" + url + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
